ef318fc8f56188372b8a44445f17a0436186c42e
Dipak Zende <114248721+dipak307@users.noreply.github.com>
2024-09-18 21:01:42 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,25 @@ import { Route, Routes } from 'react-router-dom';
 import Dashboard from './Components/Dashboard'; 
 import Home from './Components/Home';
 import Leaderboard from './Components/Leaderboards';
+import PrivateRoute from "./PrivateRoute";
 
 function App() {
+      
   return (
     <>
       <div className='App'>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/" element={<Dashboard />}>
+      {/* <Route path="/" element={<Dashboard />}>  */}
+      <Route 
+            path="/" 
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          >
             <Route index element={<Home />} /> {/* Default Route as Home */}
             <Route path="/leaderboard" element={<Leaderboard />} /> {/* Leaderboard Route */}
           </Route>
